9aac25512bfcbe881c73f490cf4905b4bb4a8c0b
DanOliverRunoLarsen <42993978+DanOliverRunoLarsen@users.noreply.github.com>
2018-12-18 08:48:39 +0100

    Opgave 1 og 2 done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-    public static final String SOFTWARE_VERSION = "0.1.0";
+    public static final String SOFTWARE_VERSION = "TV2 Media Manager";
     public static final String MEDIA_MAPPE = "media/";
 
     public static void main(String[] args) {
